02c12f07e2e108a15fd450c9e15ebceabb10b20f
Anna Arpaci-Dusseau <annaad@dhcp-10-29-245-155.dyn.mit.edu>
2023-10-25 10:31:15 -0400

    experiment 2

---
+++ manager/run-fig2-experiment-server.sh
@@ -0,0 +1,46 @@
+ifconfig | grep 'inet addr:' | cut -d: -f2
+
+
+mkdir /cgroup/cpu_and_mem/group1
+mkdir /cgroup/cpu_and_mem/group2
+
+cgset -r cpu.shares=50 /cgroup/cpu_and_mem/group1
+cgset -r cpu.shares=50 /cgroup/cpu_and_mem/group2
+
+# start server running
+cgexec -g cpu:group1 ./tasks/tcp_server/server > fig1-server.out &
+
+
+until [[ $(wc -l < fig1-server.out) -gt 5 ]]
+do
+    echo "waiting for client..."
+    sleep 1
+done
+
+# time check experiment start, wait to start chaos.
+echo $(date)
+sleep 30 
+
+echo $(date)
+echo "starting matrix multiply"
+
+
+for i in {1..$1}
+do
+    cgexec -g cpu:group2 ./tasks/matrix_multiplier/matrix 1000 1000 1000&
+    pids[${i}]=$!
+    echo $(date)
+    #sleep 10
+done
+
+echo "waiting for multiplies"
+# wait for all pids
+for pid in ${pids[*]}; do
+    wait $pid
+done
+echo $(date)
+
+
+
+
+
