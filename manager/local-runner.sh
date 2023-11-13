1585a84f2ff98c47a219328dc7e9a6b618dd0db3
Anna Arpaci-Dusseau <annaad@dhcp-10-29-236-26.dyn.mit.edu>
2023-11-13 13:39:56 -0500

    script updates

--- manager/local-runner.sh
+++ manager/local-runner.sh
@@ -11,13 +11,13 @@ then
     ssh -i ~/.ssh/cloudlab $CLIENT_NODE git -C cgroup-benchmark/ pull 
 fi
 
-if [[ $1 == "pull" || $1 == "setup" ]]
+if [[ $1 == "setup" ]]
 then
 ssh -i ~/.ssh/cloudlab $SERVER_NODE git clone https://github.com/AnnaAD/cgroup-benchmark.git
 ssh -i ~/.ssh/cloudlab $CLIENT_NODE git clone https://github.com/AnnaAD/cgroup-benchmark.git
 fi
 
-if [[ $1 == "pull" || $1 == "setup" ]]
+if [[ $1 == "pull" ]] || [[ $1 == "setup" ]]
 then
 ssh -i ~/.ssh/cloudlab $SERVER_NODE chmod +x cgroup-benchmark/manager/setup.sh 
 ssh -i ~/.ssh/cloudlab $SERVER_NODE sudo ./cgroup-benchmark/manager/setup.sh
@@ -30,7 +30,7 @@ then
 IP_ADDR=`ssh -i ~/.ssh/cloudlab $SERVER_NODE hostname -I | cut -f2 -d' '`
 echo $IP_ADDR
 ssh -i ~/.ssh/cloudlab $SERVER_NODE sudo ./cgroup-benchmark/manager/run-fig1-experiment-server.sh 1 1000&
-ssh -i ~/.ssh/cloudlab $CLIENT_NODE sudo ./cgroup-benchmark/manager/run-fig1-experiment-client.sh $IP_ADDR fig1 1
+ssh -i ~/.ssh/cloudlab $CLIENT_NODE sudo ./cgroup-benchmark/manager/run-fig1-experiment-client.sh $IP_ADDR fig1 1 0
 fi
 
 if [[ $1 == "kill" ]]
@@ -38,5 +38,6 @@ then
 ssh -i ~/.ssh/cloudlab $SERVER_NODE sudo pkill -9 -f ./cgroup-benchmark/manager/
 ssh -i ~/.ssh/cloudlab $CLIENT_NODE sudo pkill -9 -f ./cgroup-benchmark/manager/
 ssh -i ~/.ssh/cloudlab $SERVER_NODE sudo pkill -9 -f ./tasks
-ssh -i ~/.ssh/cloudlab $SERVER_NODE sudo pkill -9 -f ./monitor
+ssh -i ~/.ssh/cloudlab $CLIENT_NODE sudo pkill -9 -f ./tasks
+ssh -i ~/.ssh/cloudlab $SERVER_NODE sudo pkill -9 -f ./manager/monitor.sh
 fi
